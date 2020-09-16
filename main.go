9b2d7b1456fa475ffae59bd305ca09b2141ba6dd
SkyAo <csvwolf@qq.com>
2020-09-16 22:43:08 +0800

    feat: add redis (#2)

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"neopets/common"
 	"neopets/controllers"
 	"net/http"
 )
+
+const NeoPriceDb = 1
 
 func StartServer() {
 	http.HandleFunc("/price", controllers.PriceWizard)
@@ -11,5 +14,7 @@ func StartServer() {
 }
 
 func main() {
+	rdb := &common.Redis{Addr: "localhost:6379", Password: "", Db: NeoPriceDb}
+	rdb.Connect()
 	StartServer()
 }
--- services/shop-wizard.go
+++ services/shop-wizard.go
@@ -6,6 +6,7 @@ import (
 	"neopets/common"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -23,11 +24,17 @@ It will be helpful in jobs, quest and rob cargo.
 The result is from: https://items.jellyneo.net/
 */
 func QuickGetPrice(name string) string {
+	val, err := common.RDB.Read(name)
+	if err == nil {
+		return val
+	}
+
 	url := fmt.Sprintf(DatabaseSearchUrl, strings.Replace(name, " ", "+", -1))
 	res, err := common.Got("Get", url, nil, []*http.Cookie{})
 
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		log.Print("Try later")
 	}
 	defer res.Body.Close()
 
@@ -38,5 +45,9 @@ func QuickGetPrice(name string) string {
 
 	price := doc.Find(".price-history-link").First().Text()
 	log.Print(price)
+
+	// cache a week
+	common.RDB.Write(name, price, time.Hour*24*7)
+
 	return price
 }
